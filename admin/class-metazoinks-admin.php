33bab949415ae18679ed8690d5ce62106f19f4f5
Barry Ceelen <b@rryceelen.com>
2017-07-25 11:56:00 +0200

    Improve wpcs compliance

--- admin/class-metazoinks-admin.php
+++ admin/class-metazoinks-admin.php
@@ -76,7 +76,7 @@ class Metazoinks_Admin {
 	static function get_instance() {
 		// If the single instance hasn't been set, set it now.
 		if ( null === self::$instance ) {
-			self::$instance = new self;
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
